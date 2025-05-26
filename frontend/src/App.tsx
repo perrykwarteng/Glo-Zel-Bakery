621ed95e9c32ee28d70c3b7737bfd57e057e57fb
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-05-26 15:17:13 +0000

    entery part of the app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import HomePage from "./pages/HomePage";
+import Order from "./pages/OrderPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen bg-wheat-50">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
