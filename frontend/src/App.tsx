b309c9d045dc28e7d98b1bcbc74bc1c72d16a28f
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-05-28 12:17:01 +0000

    added verfication page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
 import Order from "./pages/OrderPage";
+import VerifyPayment from "./pages/verifyPayment";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/order" element={<Order />} />
+            <Route path="/verifyPayment" element={<VerifyPayment />} />
           </Routes>
         </main>
         <Footer />
