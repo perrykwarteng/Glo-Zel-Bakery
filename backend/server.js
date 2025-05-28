c012a9a1715a6ebcb505ad2c45bbd36ffd402534
Perry John Kwarteng <perrykwarteng8@gmail.com>
2025-05-28 12:16:12 +0000

    server connection

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.config.js";
+import paymentRoutes from "./routes/payment.route.js";
+
+await connectDB();
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 8000;
+
+app.use("/api/payments", paymentRoutes);
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port http://localhost:${PORT}`);
+});
